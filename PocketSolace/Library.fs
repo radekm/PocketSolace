c78a21cc27daefc74cd740cb08540b5f2a8aad6b
Radek Micek <radek.micek@gmail.com>
2022-07-02 23:50:52 +0200

    Initial commit

---
+++ PocketSolace/Library.fs
@@ -0,0 +1,20 @@
+﻿namespace PocketSolace
+
+open System
+open System.Threading.Tasks
+
+type ISolace =
+    abstract Subscribe : string -> Task
+
+    // TODO
+    // abstract Unsubscribe : string -> Task
+    //
+    // abstract CreateMessage : string -> IMessage
+    // abstract Send : IMessage -> Task
+
+    abstract TerminationReason : Task
+    
+    /// Set after session and context are disposed.
+    abstract Terminated : Task
+    
+    inherit IAsyncDisposable
